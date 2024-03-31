75d63f310243193e71a833eeca20f0bd99e7a0c8
okmts554 <165137471+okmts554@users.noreply.github.com>
2024-04-01 01:30:45 +0800

    upload

---
+++ selflearn/src/chap01/Hello.java
@@ -0,0 +1,15 @@
+package chap01;
+
+import java.time.LocalDateTime;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+		// TODO Auto-generated method stub
+
+	}
+
+}
